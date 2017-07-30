890c0094cda4a662e5a763162e25c2a146bd44c1
olvin.hh <olvin.hh@yandex.ru>
2017-07-30 18:57:21 +0300

    task5: created heap

---
+++ 5-Heaps. Find the Running Median.java
@@ -0,0 +1,110 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class MinIntHeap {
+    private int capacity = 10;
+    private int size = 0;
+
+    int[] items = new int[capacity];
+
+    private int getLeftChildIndex(int parentIndex) { return 2 * parentIndex + 1; }
+    private int getRightChildIndex(int parentIndex) { return 2 * parentIndex + 2; }
+    private int getParentIndex(int childIndex) { return (childIndex - 1) / 2; }
+
+    private boolean hasLeftChild(int index) { return getLeftChildIndex(index) < size; }
+    private boolean hasRightChild(int index) { return getRightChildIndex(index) < size; }
+    private boolean hasParent(int index) { return getParentIndex(index) >= 0; }
+
+    private int leftChild(int index) { return items[getLeftChildIndex(index)]; }
+    private int rightChild(int index) { return items[getRightChildIndex(index)]; }
+    private int parent(int index) { return items[getParentIndex(index)]; }
+
+    private void swap(int indexOne, int indexTwo) {
+        int temp = items[indexOne];
+        items[indexOne] = items[indexTwo];
+        items[indexTwo] = temp;
+    }
+
+    private void ensureExtraCapacity() {
+        if (size == capacity) {
+            items = Arrays.copyOf(items, capacity * 2);
+            capacity *= 2;
+        }
+    }
+
+    public int peek() {
+        if (size == 0) { throw new IllegalStateException(); }
+        return items[0];
+    }
+
+    public int poll() {
+        if (size == 0) { throw new IllegalStateException(); }
+        int item = items[0];
+        items[0] = items[size - 1];
+        size--;
+        heapifyDown();
+        return item;
+    }
+
+    public void add(int item) {
+        ensureExtraCapacity();
+        items[size] = item;
+        size++;
+        heapifyUp();
+    }
+
+    private void heapifyUp() {
+        int index = size - 1;
+        while (hasParent(index) && parent(index) > items[index]) {
+            swap(getParentIndex(index), index);
+            index = getParentIndex(index);
+        }
+    }
+
+    private void heapifyDown() {
+        int index = 0;
+        while (hasLeftChild(index)) {
+            int smallerChildIndex = getLeftChildIndex(index);
+            if (hasRightChild(index) && rightChild(index) < leftChild(index)) {
+                smallerChildIndex = getRightChildIndex(index);
+            }
+
+            if (items[index] < items[smallerChildIndex]) {
+                break;
+            } else {
+                swap(index, smallerChildIndex);
+            }
+            index = smallerChildIndex;
+        }
+    }
+}
+
+public class Solution {
+
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
+    public static void main(String[] args) {
+        MinIntHeap heap = new MinIntHeap();
+        heap.add(4);
+        heap.add(10);
+        heap.add(2);
+        heap.add(20);
+        heap.add(3);
+        heap.add(5);
+        heap.add(6);
+        heap.add(1);
+
+        for (int i = 0; i < heap.items.length; i++) {
+            System.out.println(heap.items[i]);
+        }
+    }
+}
